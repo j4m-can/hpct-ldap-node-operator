5611f343ee17872ff748049099767e8fe21ea13c
John Marshall <john.marshall@canonical.com>
2022-09-23 06:58:42 -0600

    Initial work commit

---
+++ src/charm.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""LDAP node operator.
+"""
+
+import logging
+
+from ops.main import main
+
+from hpctlib.ops.charm.node import NodeCharm
+from hpctlib.interface.relation import interface_registry
+
+
+logger = logging.getLogger(__name__)
+
+
+class HpctLDAPNodeCharm(NodeCharm):
+    """Operator for cluster LDAP node."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.interfaces = {
+            "ldap-server-ready": interface_registry.load(
+                "relation-subordinate-ready", self, "ldap-server-ready"
+            ),
+        }
+
+        self.setup_subordinate_relations_and_syncs(["ldap-server-ready"])
+
+
+if __name__ == "__main__":
+    main(HpctLDAPNodeCharm)
